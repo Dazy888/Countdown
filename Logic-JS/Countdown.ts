50dda784a9a0bc4f15400f328b7ac8a0177d5940
DacinCM <8205414@gmail.com>
2022-05-16 22:15:35 +0300

    Upgraded HTML code

--- Logic-JS/Countdown.ts
+++ Logic-JS/Countdown.ts
@@ -1,22 +1,21 @@
-// Geting All Elements
-
+// Getting All Elements
 // Buttons (Start and Back)
 const startBtn: any = document.querySelector('.start_btn')
 const backBtn: any = document.querySelector('.back_btn')
 
-// Choose Date Elements
-const chooseDateBl: any = document.querySelector('.choose_date')
+// Elements Of Choosing Date Block
+const choosingDateBl: any = document.querySelector('.choose-date')
 const dateInp: any = document.querySelector('.date_inp')
 
 // Timer Block Elements
 const timerBl: any = document.querySelector('.timer')
-const days: any = document.querySelector('.d');
-const hours: any = document.querySelector('.h');
-const minutes: any = document.querySelector('.m');
-const seconds: any = document.querySelector('.s');
+const days: any = document.querySelector('.d')
+const hours: any = document.querySelector('.h')
+const minutes: any = document.querySelector('.m')
+const seconds: any = document.querySelector('.s')
+
 
 // Event Listeners
-
 // Window
 function windowLoadAnimation() {
     startBtn.style.top = document.documentElement.clientHeight / 2 - startBtn.clientHeight + 'px'
@@ -26,65 +25,50 @@ function windowLoadAnimation() {
 // Show Choose Date Btn
 function showChooseDate() {
     startBtn.style.display = 'none'
-    chooseDateBl.style.display = 'block'
-    chooseDateBl.style.opacity = '1'
+    choosingDateBl.style.display = 'block'
+    choosingDateBl.style.opacity = '1'
     backBtn.style.display = 'block'
 }
 
 // Start Timer Btn
 function startTimer(e) {
     e.preventDefault()
+    if (dateInp.value === '') alert('Insert the date again')
 
-    if (dateInp.value === '') {
-        alert('Insert the date again')
-    } else {
-        chooseDateBl.style.display = 'none'
-        timerBl.style.display = 'block'
+    choosingDateBl.style.display = 'none'
+    timerBl.style.display = 'block'
+    const birthday: any = new Date(dateInp.value)
+    function startTimer() {
+        const date: any = new Date()
+        let ms: number
 
-        let birthday: any = new Date(dateInp.value);
-        function startTimer() {
-            let date: any = new Date();
-            let ms = 0;
+        if (birthday > date) ms = birthday - date
+        ms = 31536000000 - Date.parse(date) + Date.parse(birthday)
 
-            if (birthday > date) {
-                ms = birthday - date;
-            } else {
-                ms = 31536000000 - Date.parse(date) + Date.parse(birthday);
-            }
-
-            days.innerText = Math.round(ms / 86400000)
-            hours.innerText = 24 - date.getHours()
-            minutes.innerText = 60 - date.getMinutes()
-            seconds.innerText = 60 - date.getSeconds()
-        }
-        startTimer();
-
-        setInterval(() => {
-            startTimer();
-        }, 1000);
+        days.innerText = Math.round(ms / 86400000)
+        hours.innerText = 24 - date.getHours()
+        minutes.innerText = 60 - date.getMinutes()
+        seconds.innerText = 60 - date.getSeconds()
     }
+    startTimer()
+
+    setInterval(() => { startTimer() }, 1000)
 }
 
 // Back Btn
 function turnBack() {
-    if (chooseDateBl.style.display === 'block') {
+    if (choosingDateBl.style.display === 'block') {
         startBtn.style.display = 'block'
-        chooseDateBl.style.display = 'none'
+        choosingDateBl.style.display = 'none'
         backBtn.style.display = 'none'
     } else {
-        chooseDateBl.style.display = 'block'
+        choosingDateBl.style.display = 'block'
         timerBl.style.display = 'none'
     }
 }
 
 // Setting Event Listeners
-
-// Submit
-chooseDateBl.onsubmit = startTimer
-
-// Load
+choosingDateBl.onsubmit = startTimer
 window.onload = windowLoadAnimation
-
-// Click
 startBtn.onclick = showChooseDate
 backBtn.onclick = turnBack
